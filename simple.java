abef684975b040084a3ea9224ff9b6432fcda6cf
Dhanush3111 <dhanushhr312007@gmail.com>
2025-03-19 12:43:45 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Addition {
+class simple {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first number: ");
